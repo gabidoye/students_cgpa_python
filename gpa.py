3118b91b762e37e2ca2956cba149dc7ee04e0b79
gabidoye <86935340+gabidoye@users.noreply.github.com>
2022-09-24 21:39:52 -0600

    combined the two functions for calculating grade and point into one function

--- gpa.py
+++ gpa.py
@@ -10,11 +10,13 @@ student_record = {
    ]
 }
 
-# print(students["courses"])
+# print(student_record["courses"])
 
-def compute_student_grade(student_scores):
+def compute_student_grade(student_record,points):
+   grade_point_map = {'A': 5, 'B': 4, 'C':3, 'D':2, 'E':1, 'F':0}
    grade=[]
-   for score in student_scores:
+   grade_points=[grade_point_map[grade] for grade in points if grade in grade_point_map.keys()]
+   for score in student_record:
       if score >=70:
          grade.append('A')
       if score  >=60 and score <=69:
@@ -27,49 +29,32 @@ def compute_student_grade(student_scores):
          grade.append('E')
       if score <=39 :
          grade.append('F')
-   return grade
+   return grade ,grade_points  
+   
+Result=[60, 80, 40, 30]
+point=['B','A','D','E']
+print(compute_student_grade(Result, point))
 
-# Result=[60, 80, 40, 30]
-# print(compute_student_grade(Result))
-
-def compute_student_grade_point(student_grades):
-   points=[]
-   for grade in student_grades:
-      if grade == 'A':
-         points.append(5)
-      if grade == 'B':
-         points.append(4)
-      if grade == 'C':
-         points.append(3)
-      if grade == 'D':
-         points.append(2)
-      if grade == 'E':
-         points.append(1)
-      if grade == 'F':
-         points.append(0)
-   return points
-
-# print(compute_student_grade_point(['B', 'A']))
 
 def compute_student_grade_point_average(unit_credit, unit_score):
    sum_units= sum(unit_credit)
    sum_score= sum(unit_score)
    return round((sum_score / sum_units), 2)
 
-# print(compute_student_grade_point_average([4, 5], [8, 15]))
+# # print(compute_student_grade_point_average([4, 5], [8, 15]))
 
-def compute_student_gpa(student_record):
+# def compute_student_gpa(student_record):
     
-    cgpa= (student_record['courses'])
-    unit=[d['Unit'] for d in (student_record['courses'])] 
-    student_scores=[d['score'] for d in (student_record['courses'])] 
-    grade_scale = compute_student_grade(student_scores)
-    grade_score = compute_student_grade_point(grade_scale)
-    grade_point = [a*b for a,b in zip(unit,grade_score)]
-    grade_point_avg= compute_student_grade_point_average(unit,grade_point)
+#     cgpa= (student_record['courses'])
+#     unit=[d['Unit'] for d in (student_record['courses'])] 
+#     student_scores=[d['score'] for d in (student_record['courses'])] 
+#     grade_scale = compute_student_grade(student_scores)
+#     grade_score = compute_student_grade_point(grade_scale)
+#     grade_point = [a*b for a,b in zip(unit,grade_score)]
+#     grade_point_avg= compute_student_grade_point_average(unit,grade_point)
        
-    return grade_point_avg
-print(compute_student_gpa(students))
+#     return grade_point_avg
+# print(compute_student_gpa(students))
 
 
 
